bdee7aab4c4fbb5ecf041aa8e66637211c781480
Sameer Bahadur <bahadur.sameer@gmail.com>
2022-03-10 19:00:21 +0530

    simplified provide_liquidity authorized

--- testing/provide_liquidity.js
+++ testing/provide_liquidity.js
@@ -47,18 +47,14 @@ const question = promisify(rl.question).bind(rl);
 const main = async () => {
     try {
         console.log(`Wallet to be used treasury_wallet ${treasury_wallet.key.accAddress}`);
-        const uusdInput = await question('How much uusd tokens you wish to xrf to LP ? ');
         let deploymentDetails = readArtifact(terraClient.chainID);
-        let uusd_amount = Number(uusdInput);
-        let resp = await queryBank(treasury_wallet.key.accAddress);
-        console.log(`${JSON.stringify(resp[1].total)}  ${JSON.stringify(resp[0])}`);
-        await provideLiquidityAuthorised(deploymentDetails,uusd_amount,treasury_wallet,false);
+        await provideLiquidityAuthorised(deploymentDetails,treasury_wallet,false);
     } catch (error) {
         console.log(error);
     }
     rl.close()
 }
-const provideLiquidityAuthorised = async (deploymentDetails, uusd_amount, wallet, checkOnly) => {
+const provideLiquidityAuthorised = async (deploymentDetails, wallet, checkOnly) => {
     let proxyQuery = await queryContract(deploymentDetails.proxyContractAddress, {
                 configuration: {}
             });
@@ -69,16 +65,33 @@ const provideLiquidityAuthorised = async (deploymentDetails, uusd_amount, wallet
     let poolQuery = await queryContract(deploymentDetails.proxyContractAddress, {
                 pool: {}
             });
-    let fury_amount = Math.trunc(uusd_amount*Number(poolQuery.assets[0].amount)/Number(poolQuery.assets[1].amount));
-    console.log(`Price of 1 Fury = ${Number(poolQuery.assets[1].amount)/Number(poolQuery.assets[0].amount)} UST`);
     let furyBalanceQuery = await queryContract(deploymentDetails.furyContractAddress, {
                                                 balance: { address : wallet.key.accAddress}
                                                 });
-    if (Number(furyBalanceQuery.balance) < fury_amount || checkOnly)     {
-        console.log(`Fury Balance of ${wallet.key.accAddress} : ${furyBalanceQuery.balance}`);
-        console.log(`Fury Required ${fury_amount}`);
+    console.log(`Pool uFury = ${Number(poolQuery.assets[0].amount)} Pool uusd ${Number(poolQuery.assets[1].amount)} UST`);
+    let furyPrice = Number(poolQuery.assets[1].amount)/Number(poolQuery.assets[0].amount);
+    console.log(`Price of 1 Fury = ${furyPrice} UST`);
+    let resp = await queryBank(treasury_wallet.key.accAddress);
+    let uusd_balance = Number(resp[0]._coins.uusd.amount);
+    let fury_balance = Number(furyBalanceQuery.balance);
+    console.log(`Balance of ${wallet.key.accAddress}  uusd : ${uusd_balance}, can pe paired with uFury ${Math.trunc(uusd_balance/furyPrice)}`);
+    console.log(`           ${wallet.key.accAddress} uFury : ${fury_balance},                    uusd  ${Math.trunc(fury_balance*furyPrice)}`);
+    const uusdInput = await question('How much uusd tokens you wish to transfer to LiquidityPool ? ');
+    let uusd_amount = Number(uusdInput);
+    let fury_amount = Math.trunc(uusd_amount*Number(poolQuery.assets[0].amount)/Number(poolQuery.assets[1].amount));
+
+    if (uusd_balance < uusd_amount)     {
+        console.log(`uusd Available ${uusd_balance}`);
         return;
     }
+    if (fury_balance < fury_amount)     {
+        console.log(`uFury Required ${fury_amount}`);
+        return;
+    }
+    if (checkOnly)     {
+        return;
+    }
+
     console.log(`Fury Required ${fury_amount}`);
     // First increase allowance for proxy to spend from mint_wallet wallet
     let increaseAllowanceMsg = {
