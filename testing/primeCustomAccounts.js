a66850937e68df0cd6b14e7c16b46502567b2bad
Sameer Bahadur <bahadur.sameer@gmail.com>
2022-04-04 17:02:30 +0530

    proxy related scripts update

--- testing/primeCustomAccounts.js
+++ testing/primeCustomAccounts.js
@@ -11,6 +11,7 @@ import {
     liquidity_wallet,
     marketing_wallet,
     bonded_lp_reward_wallet,
+    team_money_wallet,
     nitin_wallet,
     ajay_wallet,
     sameer_wallet,
@@ -21,19 +22,20 @@ import { MsgSend } from '@terra-money/terra.js';
 
 export async function primeAccountsWithFunds() {
     var txHash = [];
-    txHash.push(await bankTransferFunde(walletTest1,mint_wallet,500000000,1000000000));
-    txHash.push(await bankTransferFunde(walletTest2,treasury_wallet,500000000,10000000000));
-    txHash.push(await bankTransferFunde(walletTest3,liquidity_wallet,500000000,10000000000));
-    txHash.push(await bankTransferFunde(walletTest4,marketing_wallet,500000000,10000000000));
-    txHash.push(await bankTransferFunde(walletTest5,nitin_wallet,500000000,10000000000));
-    txHash.push(await bankTransferFunde(walletTest6,ajay_wallet,500000000,10000000000));
-    txHash.push(await bankTransferFunde(walletTest7,sameer_wallet,500000000,10000000000));
-    txHash.push(await bankTransferFunde(walletTest5,bonded_lp_reward_wallet,500000000,10000000000));
+    txHash.push(await bankTransferFund(walletTest1,mint_wallet,500000000,1000000000));
+    txHash.push(await bankTransferFund(walletTest1,team_money_wallet,500000000,1000000000));
+    txHash.push(await bankTransferFund(walletTest2,treasury_wallet,500000000,10000000000));
+    txHash.push(await bankTransferFund(walletTest3,liquidity_wallet,500000000,10000000000));
+    txHash.push(await bankTransferFund(walletTest4,marketing_wallet,500000000,10000000000));
+    txHash.push(await bankTransferFund(walletTest5,nitin_wallet,500000000,10000000000));
+    txHash.push(await bankTransferFund(walletTest6,ajay_wallet,500000000,10000000000));
+    txHash.push(await bankTransferFund(walletTest7,sameer_wallet,500000000,10000000000));
+    txHash.push(await bankTransferFund(walletTest5,bonded_lp_reward_wallet,500000000,10000000000));
     console.log("leaving primeCustomAccounts");
     return txHash;
 }
 
-function bankTransferFunde(wallet_from, wallet_to, uluna_amount, uusd_amount) {
+function bankTransferFund(wallet_from, wallet_to, uluna_amount, uusd_amount) {
     console.log(`Funding ${wallet_to.key.accAddress}`);
     return new Promise(resolve => {
         // create a simple message that moves coin balances
